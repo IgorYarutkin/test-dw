328b07045c2e91b92b0c325a60d1628022e5ccda
IgorYarutkin <igor.yarutkin@gmail.com>
2016-11-11 13:52:06 +0300

    Переверстать блок табов

---
+++ src/markup/components/tabs/data/data.js
@@ -0,0 +1,17 @@
+var data = {tabs: {
+    list: [
+        {
+            id: 'opportunity',
+            title: 'Возможности',
+            active: true
+        },
+        {
+            id: 'info',
+            title: 'Информация'
+        },
+        {
+            id: 'privilege',
+            title: 'Привилегии'
+        }
+    ]
+}};
