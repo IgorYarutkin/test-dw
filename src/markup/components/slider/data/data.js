9cec84e9dae30d8d9fe49e1c59964ebe01408612
IgorYarutkin <igor.yarutkin@gmail.com>
2016-11-11 14:04:12 +0300

    Сверстать слайдер

---
+++ src/markup/components/slider/data/data.js
@@ -0,0 +1,10 @@
+var data = {slider: {
+    list: [
+        {
+            text: 'Открывайте мир',
+            title: 'вместе с Трансаэро',
+            note: 'Почувствуйте комфорт современного Boeing 747-300',
+            src: 'static/img/assets/slider/bg-slide101.jpg'
+        }
+    ]
+}};
